41ef039fb9bbdefd926a87978cdb0d8fe31ffd0f
pecandu <69785938+pecandu@users.noreply.github.com>
2020-08-23 05:19:18 +0000

    Update facebok.py

--- facebok.py
+++ facebok.py
@@ -2,12 +2,12 @@ import os
 import sys
 os.system('clear')
 os.system('figlet hack facebok')
-print "========================================"
-print "=Author  : Ahmad Rofii                 ="
-print "=facebok : Ahmad Rofii                 ="
-print "=youtube : Pecandu mimpi               ="
-print "=github  : https://github.com/pecandu  ="
-print "========================================"
+print "#======================================#"
+print "#Author  : Ahmad Rofii                 #"
+print "#facebok : Ahmad Rofii                 #"
+print "#youtube : Pecandu mimpi               #"
+print "#github  : https://github.com/pecandu  #"
+print "#======================================#"
 print
 raw_input('nomor/email : ')
-raw_input('password : ')
+raw_input('   password : ')
